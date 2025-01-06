c6da4c0abf7723b887d4a959402394b2ade955a9
Hamid Abdullah <113660005+HamidAbdullah@users.noreply.github.com>
2025-01-06 08:46:22 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,40 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/homescreen';
 import RegisterScreen from './src/auth/register';
 import SignInScreen from './src/auth/signin';
-import {AuthProvider, useAuth} from './src/context/authProvider';
+import { AuthProvider, useAuth } from './src/context/authProvider';
 import FirebaseDataUpload from './src/screens/firebaseDataUpload';
 
 const Stack = createStackNavigator();
 
 const AuthenticatedStack: React.FC = () => {
-  const {user, loading} = useAuth();
+  const { user } = useAuth();
 
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      {!user ? (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
         <>
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="SignInScreen" component={SignInScreen} />
+          <Stack.Screen name="FirebaseDataUpload" component={FirebaseDataUpload} />
+          <Stack.Screen name="Home" component={HomeScreen} />
         </>
       ) : (
         <>
-          <Stack.Screen
-            name="FirebaseDataUpload"
-            component={FirebaseDataUpload}
-          />
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="SignInScreen" component={SignInScreen} />
         </>
       )}
     </Stack.Navigator>
   );
 };
 
-const App: React.FC = () => {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <AuthenticatedStack />
-      </NavigationContainer>
-    </AuthProvider>
-  );
-};
+const App: React.FC = () => (
+  <AuthProvider>
+    <NavigationContainer>
+      <AuthenticatedStack />
+    </NavigationContainer>
+  </AuthProvider>
+);
 
 export default App;
